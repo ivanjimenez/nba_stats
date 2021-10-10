79f47900c10467cc3992a651b00e3431fa7a7d93
ivanjimenez <ivan.jimenez@iesluisbraille.es>
2021-10-10 16:46:07 +0200

    .

---
+++ prueba.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+@author: kalki
+"""
+
+import matplotlib.pyplot as plt
+
+
+print(5+5)
+x = 1
+y = 'hello'
+z = True
+
+a = [1,2,3,4,5]
+b = [2,3,5,6,2]
+
+plt.bar(a,b)
+plt.show()
